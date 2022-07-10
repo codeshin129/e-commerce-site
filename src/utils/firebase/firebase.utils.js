6683ef095d48a84b5b5e082056946a1221792883
shen jialiang <shenjialiang993@gmail.com>
2022-07-10 23:16:32 +0900

    feat: add create user document feature

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,8 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyD_3FZvJFM9lhuPqJ4Gs8Hlx7F9q7NS7Ws',
   authDomain: 'crwn-clothing-db-65e04.firebaseapp.com',
@@ -27,3 +29,30 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      throw new Error('something is wrong', error.message);
+    }
+  }
+  return userDocRef;
+};
